42ac16c9e5baecaa720205c2633a34cf7fed6e43
Олег Олейник <olegoleynik@MacBook-Air-Oleg.local>
2017-06-19 05:34:11 +0300

    Update file carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,4 @@
-if !Rails.env.rspec? || Rails.env.production?
+if Rails.env.development? || !Rails.env.rspec?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
